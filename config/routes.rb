652c584d5283042ef3064fb35d6a2569af87a630
albator1983 <kim13kara@gmail.com>
2021-11-25 17:32:37 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  root to: "home#index"
+  root to: "email#index"
   resources :tasks, except: [:show]
+
+  resources :emails do
+    put "/unread", to: "emails#unread"
+    put "/read", to: "emails#read"
+  end
   #get 'registrations#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,10 @@ require 'faker'
     my_task.save
   end
 end
+
+
+
+
+50.times do
+  Email.create(object: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 4), body: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
+end
